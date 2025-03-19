e1e3bc3b22b7a0758c30acdedd445b56f95755ee
cons1187 <146302305+cons1187@users.noreply.github.com>
2025-03-18 20:23:40 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+import fractals #not sure if this is how you import the code from the other files
+import additional_features
+#main file for running the program, could use this to contain the gui stuff as well
+#also could maybe just throw everything into one file but it might get docked for poor organization or something
