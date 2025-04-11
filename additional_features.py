ed489a1934d51e70ed7e6db972bb8e69d986ffc2
daebreads <pbrian0819@gmail.com>
2025-04-10 17:13:23 -0700

    (temporary) reset button concept

--- additional_features.py
+++ additional_features.py
@@ -1,7 +1,16 @@
 #file for adding extra features on top of the main fractal and gui stuff, could be random colors/shapes, maybe some extra user input stuff
 #could rename the file later because its a bit too broad
 import random
-import tkinter
+import tkinter as tk
+from turtle import Screen
 
 def randomizeColor():
     return "#{:06x}".format(random.randrange(0, 2 ** 24))
+
+def makeButton(x, y, txt, cmd):
+    screen = Screen()
+    canvas = screen.getcanvas()
+    button = tk.Button(canvas.master, text = txt, command = cmd)
+    button.config(bd = 0, font="Courier", padx=5, pady=5)
+    button.pack()
+    canvas.create_window(x, y, window = button)
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import fractals
 import additional_features as features
+import turtle
 #main file for running the program, could use this to contain the gui stuff as well
 #also could maybe just throw everything into one file but it might get docked for poor organization or something
 
+def reset():
+    turtle.clearscreen()
+    main()
+
 def main():
-    #fractals.Mandelbrot(True, 0, features.randomizeColor())
-    fractals.KochSnowflake(features.randomizeColor(), 3)
+    features.makeButton(-350, -275, "Reset", reset)
+
+    fractals.Mandelbrot(True, 0, features.randomizeColor())
+    #fractals.KochSnowflake(features.randomizeColor(), 3)
 
 if __name__ == '__main__':
     main()
