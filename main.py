4f01c6adbe8a20165dd939a2931eec8857a49429
cons1187 <cdjordan@cpp.edu>
2025-04-25 20:41:10 -0700

    basic idea for main menu

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ def draw():
     '''
     Draws a specified fractal
     '''
-    #fractals.Mandelbrot(True, 0, features.randomize_color())
-    #fractals.KochSnowflake(False, features.randomize_color())
-    #fractals.SierpinskiTriangle(False, features.randomize_color())
+    #if n == 0:
+    fractals.Mandelbrot(True, 0, features.randomize_color())
+    #elif n == 1:
+    fractals.KochSnowflake(False, features.randomize_color())
+    #elif n == 2:
+    fractals.SierpinskiTriangle(False, features.randomize_color())
+    #elif n == 3:
     fractals.RandomFractal(False, features.randomize_color())
 
 def reset():
@@ -36,13 +40,25 @@ def setup():
     features.make_button(-350, -275, "Draw", draw)
     features.make_button(-250, -275, "Reset", reset)
     features.make_button(-150, -275, "Invert", features.invert)
-    features.make_button(-350, -225, "Exit", done)
+    features.make_button(-350, -225, "Back", menu_setup)
+
+def menu_setup():
+    '''
+    Sets up the main menu where the user can select which fractal they would like to view
+    '''
+    features.destroy_button()
+
+    features.make_button(-100, -100, "Mandelbrot Set", setup)
+    features.make_button(-100, 100, "Koch Snowflake", setup)
+    features.make_button(100, 100, "Sierpinski Triangle", setup)
+    features.make_button(100, -100, "Random Fractal", setup)
+    features.make_button(-25, 200, "Exit Program", done)
 
 def main():
     config.init()
     screen = turtle.Screen()
     screen.setup(800, 600)
-    setup()
+    menu_setup()
     turtle.mainloop()
 
 if __name__ == '__main__':
