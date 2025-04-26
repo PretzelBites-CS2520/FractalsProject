be5b247f4c74bac294a2e53fc3b39180326ae540
cons1187 <cdjordan@cpp.edu>
2025-04-25 21:04:10 -0700

    fixed main menu, works somewhat but looks bad

--- main.py
+++ main.py
@@ -5,19 +5,22 @@ import additional_features as features
 #also could maybe just throw everything into one file but it might get docked for poor organization or something
 
 # note: if creating separate page for user to choose, maybe add a parameter (like int or char) for a case/switch branch statement
-def draw():
+def draw(n):
     reset()
     '''
     Draws a specified fractal
+    
+    Parameters:
+    n (int) - specifies the number of the fractal to be drawn
     '''
-    #if n == 0:
-    fractals.Mandelbrot(True, 0, features.randomize_color())
-    #elif n == 1:
-    fractals.KochSnowflake(False, features.randomize_color())
-    #elif n == 2:
-    fractals.SierpinskiTriangle(False, features.randomize_color())
-    #elif n == 3:
-    fractals.RandomFractal(False, features.randomize_color())
+    if n == 1:
+        fractals.Mandelbrot(True, 0, features.randomize_color())
+    elif n == 2:
+        fractals.KochSnowflake(False, features.randomize_color())
+    elif n == 3:
+        fractals.SierpinskiTriangle(False, features.randomize_color())
+    elif n == 4:
+        fractals.RandomFractal(False, features.randomize_color())
 
 def reset():
     '''
@@ -31,13 +34,16 @@ def done():
     reset()
     turtle.Screen().bye()
 
-def setup():
+def setup(n=0):
     '''
     Sets up all the buttons on the canvas
+
+    Parameters:
+    n (int) - specifies the number of the fractal to be drawn
     '''
     features.destroy_button()
 
-    features.make_button(-350, -275, "Draw", draw)
+    features.make_button(-350, -275, "Draw", lambda: draw(n))
     features.make_button(-250, -275, "Reset", reset)
     features.make_button(-150, -275, "Invert", features.invert)
     features.make_button(-350, -225, "Back", menu_setup)
@@ -46,12 +52,13 @@ def menu_setup():
     '''
     Sets up the main menu where the user can select which fractal they would like to view
     '''
+    reset()
     features.destroy_button()
 
-    features.make_button(-100, -100, "Mandelbrot Set", setup)
-    features.make_button(-100, 100, "Koch Snowflake", setup)
-    features.make_button(100, 100, "Sierpinski Triangle", setup)
-    features.make_button(100, -100, "Random Fractal", setup)
+    features.make_button(-100, -100, "Mandelbrot Set", lambda: setup(1))
+    features.make_button(-100, 100, "Koch Snowflake", lambda: setup(2))
+    features.make_button(100, 100, "Sierpinski Triangle", lambda: setup(3))
+    features.make_button(100, -100, "Random Fractal", lambda: setup(4))
     features.make_button(-25, 200, "Exit Program", done)
 
 def main():
